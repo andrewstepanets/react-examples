fc9d8e2c9b0bd9509545ad269c348d66e167c941
Andrii Stepanets <ovrass@gmail.com>
2020-07-08 18:03:47 +0300

    Simple example of using reducer

--- redux-sandbox/src/index.js
+++ redux-sandbox/src/index.js
@@ -1,1 +1,20 @@
-console.log("Hello world");
+
+const reducer = (state = 0, action) => {
+
+  switch(action.type) {
+    case 'INC':
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+let state = reducer(undefined, {});
+console.log(state);
+
+
+state = reducer(state, { type: 'INC' });
+console.log(state);
+
+state = reducer(state, { type: 'INC' });
+console.log(state);
