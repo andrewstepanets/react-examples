6e223f96e973f0dd1ffdb3f39c28fde6b747812d
Andrii Stepanets <ovrass@gmail.com>
2020-07-08 19:21:51 +0300

    Simple UI Redux app

--- redux-sandbox/src/index.js
+++ redux-sandbox/src/index.js
@@ -6,23 +6,30 @@ const reducer = (state = 0, action) => {
   switch(action.type) {
     case 'INC':
       return state + 1;
+    case 'DEC':
+      return state - 1;
     default:
       return state;
   }
 };
 
 const store = createStore(reducer);
-// initial State our app
-console.log(store.getState());
 
-// get information aboute updating store
-store.subscribe(() => {
-  console.log(store.getState());
-});
+document
+    .getElementById('inc')
+    .addEventListener('click', () => {
+      store.dispatch({ type: 'INC' });
+    });
+document
+    .getElementById('dec')
+    .addEventListener('click', () => {
+      store.dispatch({ type: 'DEC' });
+    });
 
+    const update = () => {
+      document
+        .getElementById('counter')
+        .innerHTML = store.getState();
+    };
 
-// increment + 1
-
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
-
+    store.subscribe(update);
