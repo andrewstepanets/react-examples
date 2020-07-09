b8ee39b0c1b3ef5d3ce214cd5379b8a3e697193b
Andrii Stepanets <ovrass@gmail.com>
2020-07-09 15:21:12 +0300

    Added action creator funcs and created ploject list

--- redux-sandbox/src/index.js
+++ redux-sandbox/src/index.js
@@ -1,39 +1,26 @@
 import { createStore } from 'redux';
-
-
-const reducer = (state = 0, action) => {
-
-  switch(action.type) {
-    case 'RND':
-      return state + action.payload;
-    case 'INC':
-      return state + 1;
-    case 'DEC':
-      return state - 1;
-    default:
-      return state;
-  }
-};
+import reducer from './reducer';
+import { inc, dec, rnd } from './actions';
 
 const store = createStore(reducer);
+
+
 
 document
     .getElementById('inc')
     .addEventListener('click', () => {
-      store.dispatch({ type: 'INC' });
+      store.dispatch(inc());
     });
 document
     .getElementById('dec')
     .addEventListener('click', () => {
-      store.dispatch({ type: 'DEC' });
+      store.dispatch(dec());
     });
 document
     .getElementById('rnd')
     .addEventListener('click', () => {
       const payload = Math.floor(Math.random() * 10);
-      store.dispatch({ 
-            type: 'RND',
-            payload });
+      store.dispatch(rnd(payload));
     });
 
     const update = () => {
