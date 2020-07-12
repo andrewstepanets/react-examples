58d9f741023cfdbf4fd8da629bb43438ab883441
Andrii Stepanets <ovrass@gmail.com>
2020-07-12 17:16:00 +0300

    Added styling and sending date to Reduxe store

--- re-store/src/components/book-list/book-list.js
+++ re-store/src/components/book-list/book-list.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import BookListItem from '../book-list-item';
 import { connect } from 'react-redux';
+
+import { withBookstoreService } from '../hoc';
+import { booksLoaded } from '../../actions';
+import { compose } from '../../utils';
 import './book-list.css';
 
 class BookList extends Component {
+
+
+    componentDidMount(){
+        // 1. receive data
+        const { bookstoreService } = this.props;
+        const data = bookstoreService.getBooks();
+        
+        // 2. dispatch action to store
+        this.props.booksLoaded(data);
+    }
+
         render() {
             const { books } = this.props;    
             return(
-                <ul>
+                <ul className="book-list">
                     {
                         books.map((book) => {
                            return <li key={book.id}><BookListItem book={book}/></li>
@@ -22,4 +37,11 @@ const mapStateToProps = ({ books }) => {
     return { books };
 };
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = {
+    booksLoaded
+};
+
+export default compose(
+    withBookstoreService(),
+    connect(mapStateToProps, mapDispatchToProps)
+)(BookList); 
--- re-store/src/components/pages/home-page.js
+++ re-store/src/components/pages/home-page.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import BookList from '../book-list';
+import ShoppingCartTable from '../shopping-cart-table';
 
 const HomePage = () => {
     const books = [];
     return(
-        <BookList />
+        <div>
+            <BookList />
+            <ShoppingCartTable />
+        </div>
     );
 };
 
