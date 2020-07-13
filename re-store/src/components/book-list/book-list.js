46fca6d405e501faa20541a7e625cb71ab66b3cb
Andrii Stepanets <ovrass@gmail.com>
2020-07-13 15:56:13 +0300

    Refactored componentDidMount, used ownProps

--- re-store/src/components/book-list/book-list.js
+++ re-store/src/components/book-list/book-list.js
@@ -13,12 +13,7 @@ class BookList extends Component {
 
 
     componentDidMount(){
-        // 1. receive data
-        const { bookstoreService, booksLoaded, booksRequested, booksError } = this.props;
-        booksRequested();
-        bookstoreService.getBooks()
-            .then((data) => booksLoaded(data))
-            .catch((err) => booksError(err));
+        this.props.fetchBooks();
     }
 
         render() {
@@ -45,10 +40,16 @@ const mapStateToProps = ({ books, loading, error }) => {
     return { books, loading, error };
 };
 
-const mapDispatchToProps = {
-    booksLoaded,
-    booksRequested,
-    booksError
+const mapDispatchToProps = (dispatch, ownProps) => {
+    const { bookstoreService } = ownProps;
+    return {
+        fetchBooks: () => {
+            dispatch(booksRequested());
+            bookstoreService.getBooks()
+                .then((data) => dispatch(booksLoaded(data)))
+                .catch((err) => dispatch(booksError(err)));
+        }
+    }
 };
 
 export default compose(
