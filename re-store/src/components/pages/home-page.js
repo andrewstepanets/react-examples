4c0d058837de17db5a69003cfd14a9460f0f3949
Andrii Stepanets <ovrass@gmail.com>
2020-07-16 20:22:38 +0300

    Using middleware funcs

--- re-store/src/components/pages/home-page.js
+++ re-store/src/components/pages/home-page.js
@@ -3,7 +3,6 @@ import BookList from '../book-list';
 import ShoppingCartTable from '../shopping-cart-table';
 
 const HomePage = () => {
-    const books = [];
     return(
         <div>
             <BookList />
--- re-store/src/store.js
+++ re-store/src/store.js
@@ -1,34 +1,53 @@
-import { createStore, compose } from 'redux';
+// import { createStore, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducers';
 
-const stringEnhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
-    const originalDispatch = store.dispatch;
-    store.dispatch = (action) => {
 
-        if (typeof action === 'string') {
-            return originalDispatch({
-                type: action
-            });
-        }
-        return originalDispatch(action);
-    };
-    return store;
+
+const logMiddleware = ({ getState }) => (next) => (action) => {
+    console.log(action.type, getState());
+    return next(action);
 };
-const logEnhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
-    const originalDispatch = store.dispatch;
-    store.dispatch = (action) => {
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        });
+    }
+    return next(action);
+};
 
-        console.log(action.type);
+// const stringEnhancer = (createStore) => (...args) => {
+//     const store = createStore(...args);
+//     const originalDispatch = store.dispatch;
+//     store.dispatch = (action) => {
+
+//         if (typeof action === 'string') {
+//             return originalDispatch({
+//                 type: action
+//             });
+//         }
+//         return originalDispatch(action);
+//     };
+//     return store;
+// };
+
+// const logEnhancer = (createStore) => (...args) => {
+//     const store = createStore(...args);
+//     const originalDispatch = store.dispatch;
+//     store.dispatch = (action) => {
+
+//         console.log(action.type);
         
-        return originalDispatch(action);
-    };
-    return store;
-};
+//         return originalDispatch(action);
+//     };
+//     return store;
+// };
 
 
-const store = createStore(reducer, compose(stringEnhancer, logEnhancer));
+// run this func when we use enhancer func
+// const store = createStore(reducer, compose(stringEnhancer, logEnhancer));
+const store = createStore(reducer, applyMiddleware(stringMiddleware, logMiddleware));
 store.dispatch('HELLO_WORLD');
 
 export default store;
