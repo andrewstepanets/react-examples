0ac74f0963fc4412bfb72ca440e360c264709b01
Andrii Stepanets <ovrass@gmail.com>
2020-07-20 19:09:12 +0300

    Added babel loader

--- build-sandbox-webpack/src/index.js
+++ build-sandbox-webpack/src/index.js
@@ -1,13 +1,16 @@
-import Log from './log';
-import Calc from './calc';
-import img from './react.png';
+// import Log from './log';
+// import Calc from './calc';
+// import img from './react.png';
+// const calc = new Calc();
+// const log = new Log();
+// log.log(calc.add(1,2,3));
+// const el = document.createElement('img');
+// el.src = img;
+// document.body.appendChild(el);
 
-const calc = new Calc();
-const log = new Log();
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const App = () => <h1>Hello world</h1>;
 
-log.log(calc.add(1,2,3));
-
-const el = document.createElement('img');
-el.src = img;
-document.body.appendChild(el);
+ReactDOM.render(<App />, document.getElementById('root'));
