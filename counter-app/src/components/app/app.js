b2169f02f2005c557f3f281264ad16dac27b3e9a
Andrii Stepanets <ovrass@gmail.com>
2020-08-12 22:27:35 +0300

    Used react hook useState

--- counter-app/src/components/app/app.js
+++ counter-app/src/components/app/app.js
@@ -1,10 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Header from '../header';
 import Button from '../button';
 
-export default class App extends Component {
-    render(){
-        let count = 0;
+const App = () => {
+    return(
+        <div>
+            <Counter />
+        </div>
+    );
+};
+
+const Counter = () => {
+
+        // let count = 0;
+        const [ count, setCount ] = useState(0);
+        const incrementCount = value => {
+            setCount(count + value)
+        }
+        const decrementCount = value => {
+            setCount(count - value)
+        }
         return (
             <div className="container">
                 <div className="row">
@@ -12,10 +27,57 @@ export default class App extends Component {
                     </div>
                     <div className="col-md-10 text-center">
                         <Header />
-                        <Button increment={1} styleName={"btn btn-warning"}/>
-                        <Button increment={10} styleName={"btn btn-info"}/>
-                        <Button increment={100} styleName={"btn btn-success"}/>
-                        <Button increment={1000} styleName={"btn btn-danger"}/>
+                        <Button 
+                            value={1}
+                            symbol={"+"}
+                            styleName={"btn btn-warning"} 
+                            onClickFunction={incrementCount}/>
+                        <Button
+                            value={10}
+                            symbol={"+"}
+                            styleName={"btn btn-info"}
+                            onClickFunction={incrementCount}
+                            />
+                        <Button
+                            value={100}
+                            symbol={"+"}
+                            styleName={"btn btn-success"}
+                            onClickFunction={incrementCount}
+                            />
+                        <Button 
+                            value={1000}
+                            symbol={"+"}
+                            styleName={"btn btn-danger"}
+                            onClickFunction={incrementCount}
+                            />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="offset-md-2"></div>
+                    <div className="col-md-10 text-center">
+                        <Button
+                            value={1}
+                            symbol={"-"}
+                            styleName={"btn btn-warning"}
+                            onClickFunction={decrementCount} />
+                        <Button
+                            value={10}
+                            symbol={"-"}
+                            styleName={"btn btn-info"}
+                            onClickFunction={decrementCount}
+                        />
+                        <Button
+                            value={100}
+                            symbol={"-"}
+                            styleName={"btn btn-success"}
+                            onClickFunction={decrementCount}
+                        />
+                        <Button
+                            value={1000}
+                            symbol={"-"}
+                            styleName={"btn btn-danger"}
+                            onClickFunction={decrementCount}
+                        />
                     </div>
                 </div>
                 <div className="row">
@@ -25,7 +87,15 @@ export default class App extends Component {
                         <h2>{count}</h2>
                     </div>
                 </div>
+                <div className="row">
+                    <div className="offset-md-2">
+                    </div>
+                    <div className="col-md-10 text-center">
+                        <button className="btn btn-dark" onClick={() => setCount(0)}>Reset</button>
+                    </div>
+                </div>
             </div>
         );
-    }
-}
+};
+
+export default App;
--- counter-app/src/components/button/button.js
+++ counter-app/src/components/button/button.js
@@ -1,8 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './button.css'
 
 
-const Button = (props) =>  {
-        return <button className={props.styleName}>+ {props.increment}</button>
+const Button = ({ styleName, symbol, value, onClickFunction }) =>  {
+    const handleClick = () => {
+        onClickFunction(value)
+    }
+        return (
+            <button 
+                className={styleName}
+                onClick={handleClick}>{symbol} {value}</button>)
 }
 
 export default Button;
