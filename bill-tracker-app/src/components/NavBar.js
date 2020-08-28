e4b6a97effd0ecec1b71a606f965061e048590a1
Andrii Stepanets <ovrass@gmail.com>
2020-08-28 22:28:34 +0300

    Added styles to categories list

--- bill-tracker-app/src/components/NavBar.js
+++ bill-tracker-app/src/components/NavBar.js
@@ -4,16 +4,23 @@ const NavBar = (props) => {
     const triggerShowAddCategory = () => {
         props.showAddCategory()
     }
+    const liStyle = 'p-4 inline bg-grey-lighter hover:bg-grey-light uppercase font-black cursor-pointer';
     return(
         <div>
-            <ul>
+            <ul className="list-reset inline flex justify-center border-b-4 mb-0">
                 {props.categories ? props.categories.map((value, index) => {
-                    return <li key={index}>{value}</li>
+                    return (
+                        <li 
+                            key={index}
+                            className={liStyle}>
+                            {value}
+                        </li>
+                        )
                 })
                     : '<li>No categories</li>'}
             </ul>
             <button 
-                className="flex-no-shrink p-2 border-2 rounded bg-teal-400 text-white border-teal hover:text-white hover:bg-teal"
+                className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded"
                 onClick={triggerShowAddCategory}>
                 +
             </button>
