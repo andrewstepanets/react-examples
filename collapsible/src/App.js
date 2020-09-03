c7bcaf10bc0cbbb1354712cd589afa6e34801e9a
Andrii Stepanets <ovrass@gmail.com>
2020-09-03 16:56:46 +0300

    created basic structure

--- collapsible/src/App.js
+++ collapsible/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import image from './images/logo-01.svg';
 import { AppContainer } from './App.styles';
+import { Collapsible } from './components';
 
-function App() {
+
+const App = () => {
   return (
     <AppContainer>
       <header>
         <img src={image} alt="Logo"/>
         <h1>Collapsible</h1>
-        
+        <Collapsible />
       </header>
     </AppContainer>
   );
---
+++ collapsible/src/components/Collapsible/Collapsible.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Collapsible = () => {
+    return(
+        <div>New Collapsible</div>
+    )
+}
+
+export default Collapsible;
