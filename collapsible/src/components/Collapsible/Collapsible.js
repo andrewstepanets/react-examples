a4a675d97b0ca593ab618222f579b6901b003614
Andrii Stepanets <ovrass@gmail.com>
2020-09-03 18:27:01 +0300

    Styled components

--- collapsible/src/components/Collapsible/Collapsible.js
+++ collapsible/src/components/Collapsible/Collapsible.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { CollapsibleWrapper, Title, Panel } from './Collapsible.styles';
+import { items } from '../../data';
+
+
+const Item = ({ item }) => {
+    const { title, content } = item;
+    return(
+        <>
+            <Title>{ title }</Title>
+            <Panel>
+                <div>
+                    {content}
+                </div>
+            </Panel>
+        </>
+    )
+}
 
 export const Collapsible = () => {
     return(
-        <div>New Collapsible</div>
+        <CollapsibleWrapper>
+            {
+                 items.map(item => <Item key={item.title} item={item} />)
+            }
+        </CollapsibleWrapper>
     )
 }
 
